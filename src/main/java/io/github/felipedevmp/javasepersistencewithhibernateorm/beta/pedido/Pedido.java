b506c9125d3f4eaf2a1a0a989236bf0b0659e549
Felipe Madureira <felipepmadureira@gmail.com>
2025-07-19 09:47:26 -0300

    add Getters/Setters e hashCode/equals/toString

--- src/main/java/io/github/felipedevmp/javasepersistencewithhibernateorm/beta/pedido/Pedido.java
+++ src/main/java/io/github/felipedevmp/javasepersistencewithhibernateorm/beta/pedido/Pedido.java
@@ -10,8 +10,6 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
-
-
 @Entity
 public class Pedido 
         extends ProjectEntity
@@ -33,7 +31,74 @@ public class Pedido
     
     @Column(nullable = false, length = 20)
     private StatusPedido status;
-     
+
     
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public String getListaDeMateriaisUsados() {
+        return listaDeMateriaisUsados;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setListaDeMateriaisUsados(String listaDeMateriaisUsados) {
+        this.listaDeMateriaisUsados = listaDeMateriaisUsados;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="hashCode/equals/toString">
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + Objects.hashCode(this.cliente);
+        hash = 47 * hash + Objects.hashCode(this.listaDeMateriaisUsados);
+        hash = 47 * hash + Objects.hashCode(this.status);
+        return hash;
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        return hashCode() == obj.hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + getId() +
+                ", cliente='" + cliente + '\'' +
+                ", listaDeMateriaisUsados='" + listaDeMateriaisUsados + '\'' +
+                ", status=" + status +
+                '}';
+    }
+    
+    //</editor-fold>
     
 }
