3e5f0e3fc7f4ce7d8a85886ae17724090fe14860
Felipe Madureira <felipepmadureira@gmail.com>
2025-07-19 09:36:51 -0300

    fazendo o Crud básico

--- src/main/java/io/github/felipedevmp/javasepersistencewithhibernateorm/beta/pedido/Pedido.java
+++ src/main/java/io/github/felipedevmp/javasepersistencewithhibernateorm/beta/pedido/Pedido.java
@@ -1,10 +1,39 @@
-
 package io.github.felipedevmp.javasepersistencewithhibernateorm.beta.pedido;
 
-/**
- *
- * @author felip
- */
-public class Pedido {
+import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.ProjectEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.Transient;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+
+
+@Entity
+public class Pedido 
+        extends ProjectEntity
+            implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    public enum StatusPedido {
+        EM_PRODUCAO,
+        FINALIZADO,
+        CANCELADO
+    }
+     
+    @Column (nullable = false, length = 100)
+    private String cliente;
+     
+    @Column (length = 520)
+    private String listaDeMateriaisUsados;
+    
+    @Column(nullable = false, length = 20)
+    private StatusPedido status;
+     
+    
     
 }
