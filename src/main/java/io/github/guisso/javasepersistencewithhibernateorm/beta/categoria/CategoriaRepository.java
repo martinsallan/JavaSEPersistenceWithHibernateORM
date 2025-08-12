558533f208757bfaf1aa4d38f1e91e8300713d1d
Tefo02 <stefanolopes84@gmail.com>
2025-08-12 20:09:46 -0300

    Correção de erros

--- src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/categoria/CategoriaRepository.java
+++ src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/categoria/CategoriaRepository.java
@@ -3,6 +3,7 @@ package io.github.guisso.javasepersistencewithhibernateorm.beta.categoria;
 import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.DataSourceFactory;
 import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.Repository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
 
 /**
@@ -34,5 +35,65 @@ public class CategoriaRepository
                     .setParameter("nome", "%" + nome + "%")
                     .getResultList();
         }
+    }
+    
+    public void softDelete(Long id) {
+        try (EntityManager em = DataSourceFactory.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+
+            Categoria c = em.find(Categoria.class, id);
+            if (c != null) {
+                c.setAtivo(false); // Marca como inativo
+                em.merge(c);
+            }
+
+            tx.commit();
+        }
+    }
+
+    public void softDelete(Categoria categoria) {
+        softDelete(categoria.getId());
+    }
+
+    public void restore(Long id) {
+        try (EntityManager em = DataSourceFactory.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+
+            Categoria c = em.find(Categoria.class, id);
+            if (c != null) {
+                c.setAtivo(true); // Marca como ativo
+                em.merge(c);
+            }
+
+            tx.commit();
+        }
+    }
+
+    public void restore(Categoria categoria) {
+        restore(categoria.getId());
+    }
+    
+    public List<Categoria> findAllInTrash() {
+        try (EntityManager em = DataSourceFactory.getEntityManager()) {
+            return em.createQuery("SELECT c FROM Categoria c WHERE c.ativo = false", Categoria.class)
+                    .getResultList();
+        }
+    }
+
+    public List<Categoria> findAllActive() {
+        try (EntityManager em = DataSourceFactory.getEntityManager()) {
+            return em.createQuery("SELECT c FROM Categoria c WHERE c.ativo = true", Categoria.class)
+                    .getResultList();
+        }
+    }
+
+    public void hardDelete(Long id) {
+        super.delete(id);
+    }
+
+    public void hardDelete(Categoria categoria) {
+        hardDelete(categoria.getId());
     }
 }
