872e6b5505bb6680a98b5709dd2abc22816b8967
Allan Martins <martinsallan111@gmail.com>
2025-08-11 22:22:10 -0300

    Correções de arquivos

--- src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/fornecedor/gui/exibirFornecedor.java
+++ src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/fornecedor/gui/exibirFornecedor.java
@@ -493,6 +493,9 @@ public class exibirFornecedor extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(exibirFornecedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
