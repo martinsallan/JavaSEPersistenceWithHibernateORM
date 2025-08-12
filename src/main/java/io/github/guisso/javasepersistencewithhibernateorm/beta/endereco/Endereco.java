ad96e9908959f41b2125be303f766606230b6387
Allan Martins <martinsallan111@gmail.com>
2025-08-11 21:23:29 -0300

    Implementa a entidade Endereco e o seu Repositorio

--- src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/endereco/Endereco.java
+++ src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/endereco/Endereco.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 marti
+ * Copyright (C) 2025 Allan Martins
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,10 +16,126 @@
  */
 package io.github.guisso.javasepersistencewithhibernateorm.beta.endereco;
 
+import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.ProjectEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
- *
- * @author marti
+ * Fornecedor entity
+ * 
+ * @author Allan Martins
  */
-public class Endereco {
+
+@Entity
+public class Endereco 
+        extends ProjectEntity 
+        implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+    
+    @Column(nullable = false, length = 120)
+    private String rua;
+    
+    @Column(nullable = false, length = 120)
+    private String numero;
+    
+    @Column(nullable = false, length = 120)
+    private String bairro;
+    
+    @Column(nullable = false, length = 120)
+    private String cidade;
+    @Column(nullable = false, length = 120)
+    
+    private String cep;
+    @Column(nullable = false)
+    
+    private Boolean excluido = false;
+    
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Boolean getExcluido() {
+        return excluido;
+    }
+
+    public void setExcluido(Boolean excluido) {    
+        this.excluido = excluido;
+    }
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="hashCode/equals/toString">
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.rua);
+        hash = 29 * hash + Objects.hashCode(this.numero);
+        hash = 29 * hash + Objects.hashCode(this.bairro);
+        hash = 29 * hash + Objects.hashCode(this.cidade);
+        hash = 29 * hash + Objects.hashCode(this.cep);
+        hash = 29 * hash + Objects.hashCode(this.excluido);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return hashCode() == obj.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" 
+                + "Rua = " + rua 
+                + ", Numero = " + numero 
+                + ", Bairro = " + bairro
+                + ", Cidade = " + cidade 
+                + ", CEP = " + cep + "}"
+                + ", " + (excluido ? "Excluido " : "Valido "); 
+    }
+    
+    //</editor-fold>
 }
--- src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/endereco/EnderecoRepository.java
+++ src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/endereco/EnderecoRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 marti
+ * Copyright (C) 2025 Allan Martins;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,10 +16,99 @@
  */
 package io.github.guisso.javasepersistencewithhibernateorm.beta.endereco;
 
+import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.Repository;
+import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.DataSourceFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
 /**
+ * Repository for Fornecedor operations
  *
- * @author marti
+ * @author Allan Martins;
  */
-public class EnderecoRepository {
+
+public class EnderecoRepository
+        extends Repository<Endereco> {
+
+    @Override
+    public String getJpqlFindAll() {
+        return "SELECT a FROM Endereco a";
+    }
+
+    @Override
+    public String getJpqlFindById() {
+        return "SELECT a FROM Endereco a WHERE a.id = :id";
+    }
+
+    @Override
+    public String getJpqlDeleteById() {
+        return "DELETE FROM Endereco a WHERE a.id = :id";
+    }
+
+    //Métodos da Lixeira
+    public void softDelete(Long id){
+        try(EntityManager em = DataSourceFactory.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            
+            Endereco e = em.find(Endereco.class, id);
+            if(e != null){
+                e.setExcluido(Boolean.TRUE);
+                em.merge(e);
+            }
+            tx.commit();
+        }
+    }
     
+    public void softDelete(Endereco endereco){
+        softDelete(endereco.getId());
+    }
+    
+    public void restore(Long id){
+        try(EntityManager em = DataSourceFactory.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            Endereco e = em.find(Endereco.class, id);
+            if(e != null){
+                e.setExcluido(Boolean.FALSE);
+                em.merge(e);
+            }
+            tx.commit();
+        }
+    }
+    
+    public void restore(Endereco endereco){
+        restore(endereco.getId());
+    }
+    
+    public List<Endereco> findALLInTrash(){
+        try(EntityManager em = DataSourceFactory.getEntityManager()){
+            return em.createQuery("SELECT a FROM Endereco a WHERE a.excluido = true",
+                    Endereco.class).getResultList();
+        }
+    }
+    
+    public void hardDelete(Long id){
+        super.delete(id);
+    }
+    
+    public void hardDelete(Endereco e){
+        hardDelete(e.getId());
+    }
+    
+    public List<Endereco> findAllActive(){
+         try (EntityManager em = DataSourceFactory.getEntityManager()) {
+            return em.createQuery("SELECT a FROM Endereco a WHERE a.excluido = false", Endereco.class)
+                    .getResultList();
+        }
+    }
+    
+    public List<Endereco> findByCEP(String cep) {
+        try (EntityManager em = DataSourceFactory.getEntityManager()) {
+            String jpql = "SELECT a FROM Endereco a WHERE LOWER(a.cep) LIKE LOWER(:cep) AND a.excluido = false";
+            return em.createQuery(jpql, Endereco.class)
+                    .setParameter("cep", "%" + cep + "%")
+                    .getResultList();
+        }
+    }
 }
